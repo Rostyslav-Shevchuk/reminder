b3d7c532ac34daaa996792e3886773fa06f18f02
Rostyslav <159663154+Rostyslav-Shevchuk@users.noreply.github.com>
2024-03-05 23:03:15 +0200

    Initial Commit

---
+++ reminder/reminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  reminderApp.swift
+//  reminder
+//
+//  Created by Ростислав on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct reminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
